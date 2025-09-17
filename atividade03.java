19b3e7bc00e20a1944e305ba0969ff09fde492c3
Gustavo10508 <0001140340@senaimgaluno.com.br>
2025-09-16 21:34:28 -0300

    Ativade03_Gustavo

---
+++ atividade03.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula3;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Aula3 {
+    
+ 
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+//        Scanner entrada = new Scanner(System.in);
+//        
+//        int acucar,porcoes;
+//        
+//        acucar = 25;
+//        porcoes = 1;
+//        System.out.println("Insira a quantidade de porções:");
+//        porcoes = entrada.nextInt();
+//        System.out.println( "insira " + acucar*porcoes + " gramas de açucar");
+
+
+//          int impressora,pagina, total,pag_min;
+//          impressora = entrada.nextInt;
+//          
+//          
+//          tempo = 12;
+//          System.out.println("insira a quantidade de impressoras:");
+//          impressora = entrada.nextInt();
+//          System.out.println(tempo*impressora);
+//        
+    }
+    
+}
