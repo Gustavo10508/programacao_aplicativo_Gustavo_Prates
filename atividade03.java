45b0a04a78cb44b5e494f15e3cdc77f104c5b8ea
Gustavo10508 <0001140340@senaimgaluno.com.br>
2025-09-18 19:24:02 -0300

    Update atividade03.java

--- atividade03.java
+++ atividade03.java
@@ -2,26 +2,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package aula3;
-
-import java.util.Scanner;
-import java.lang.Math;
+package atividade03;
 
 /**
  *
  * @author Aluno
  */
-public class Aula3 {
-    
- 
-    
+import java.util.Scanner;
+import java.lang.Math;
+import static java.lang.Math.sqrt;
+
+public class Atividade03 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // TODO code application logic here
         
-//        Scanner entrada = new Scanner(System.in);
+                    //ATIVIDADE 1
+                    
+       Scanner entrada = new Scanner(System.in);
 //        
 //        int acucar,porcoes;
 //        
@@ -31,16 +32,207 @@ public class Aula3 {
 //        porcoes = entrada.nextInt();
 //        System.out.println( "insira " + acucar*porcoes + " gramas de açucar");
 
+                
 
-//          int impressora,pagina, total,pag_min;
-//          impressora = entrada.nextInt;
-//          
-//          
-//          tempo = 12;
-//          System.out.println("insira a quantidade de impressoras:");
-//          impressora = entrada.nextInt();
-//          System.out.println(tempo*impressora);
+                    //ATIVIDADE 2
+//                    
+//          int impressora,pagina,pag_min,total;
 //        
-    }
+//          pagina= 180;
+//          pag_min= 5;
+//                  
+//          System.out.println("Diginte o numero de impressoras:  ");
+//          impressora = entrada.nextInt();
+//          total =180/ (impressora*pag_min);
+//          
+//          System.out.println(total  +" minutos para imprimir 180 paginas");
+//          
+
+                    //ATIVIDADE 3
+                    
+//            double litros,distancia,litros_km;
+//            
+//            litros = 0.075;
+//            System.out.println("coloque a distancia que será percorrida e mostrarei quantos litros será necessario: ");
+//            distancia = entrada.nextDouble();
+//            litros_km = (litros*distancia);
+//            System.out.println(litros_km + " litros");
+
+
+                       //ATIVIDADE 4
+                       
+//              int pecas, horas, maquinas, pecas_por_horas;
+//              
+//              pecas=30;
+//              System.out.println("insira quantas maquinas sera utilizadas: ");
+//              maquinas = entrada.nextInt();
+//              System.out.println("insira as horas: ");
+//              horas = entrada.nextInt();
+//              pecas_por_horas = (pecas*maquinas)*horas;
+//              System.out.println(pecas_por_horas + " peças!");
+
+
+                        //ATIVIDADE 5
+                
+//                double cotacao, quantia, reais;
+//                
+//                System.out.println("insira a cotação do dollar: ");
+//                cotacao = entrada.nextDouble();
+//                System.out.println("insira a quantidade de dollar que quer levar: ");
+//                quantia = entrada.nextDouble();
+//                reais = (cotacao*quantia);
+//                System.out.println(reais + " em reais");
+
+
+                          //ATIVIDADE 6
+                          
+//                   int a,b, ax, x;
+//                   
+//                   System.out.println(" insira a: ");
+//                   a = entrada.nextInt();
+//                   System.out.println(" insira b: ");
+//                   b = entrada.nextInt();
+//                   
+//                   x = (-b/a);
+//                   
+//                   ax = (x*a)+b;
+//                   
+//                   System.out.println("A:"+a+" b:"+b+" x:"+x+" AX:"+ax);
+
+
+                           //ATIVIDADE 7
+//                         
+//                    int m, b, x0, y0;
+//                    
+//                    System.out.println("insira m: ");
+//                    m = entrada.nextInt();
+//                    System.out.println("insira b: ");
+//                    b = entrada.nextInt();
+//                    System.out.println("insira x0: ");
+//                    x0 = entrada.nextInt();
+//                    
+//                    y0= (m*x0)+b;
+//                    System.out.println(y0);
+
+
+                               //ATIVIDADE 8
+                               
+                               
+//                     int x1,y1,x2,y2,m,b,y;          
+//                     
+//                     System.out.println("insira x1: ");
+//                     x1 = entrada.nextInt();
+//                     System.out.println("insira y1");
+//                     y1 = entrada.nextInt();
+//                     System.out.println("insira x2: ");
+//                     x2 = entrada.nextInt();
+//                     System.out.println("insira y2: ");
+//                     y2 = entrada.nextInt();
+//                     
+//                     m = (y1-y2)/(x1-x2);
+//                     b = (y1-m)*x1;
+//                     y = (m*x1)+b;
+//                    System.out.println(y);     
+
+
+                                  //ATivIDADE 9
+//                                  
+////                                  
+//                       int m1, b1, m2, b2, x, y;
+//                        System.out.println("insira m1: ");
+//                     m1 = entrada.nextInt();
+//                     System.out.println("insira b1");
+//                     b1 = entrada.nextInt();
+//                     System.out.println("insira m2: ");
+//                     m2 = entrada.nextInt();
+//                     System.out.println("insira b2: ");
+//                     b2 = entrada.nextInt();
+//                     
+//                     x = (b2-b1)/(m1-m2);
+//                     y = (m1*x)+b1;
+//                     
+//                     System.out.println("resultado e: "+x + y);
+
+
+                                //Atividade 10
+                                
+                        
+                                
+//                     int a, b ,c, delta, x1, x2;
+//                     
+//                     System.out.println("insira a: ");
+//                     a = entrada.nextInt();
+//                     System.out.println("insira b");
+//                     b = entrada.nextInt();
+//                     System.out.println("insira c: ");
+//                     c = entrada.nextInt();
+//                     
+//                     delta = (b*b)-(4*a*c);
+//                     x1 = (int) ((-b + sqrt(delta))/(2*a));
+//                     x2 = (int) ((-b + sqrt(delta))/(2*a));
+//                     
+//                     System.out.println(" delta:"+delta +" x1:"+x1+" x2:" + x2);
+
+
+                                 //ATIVIDADE 11
+                                 
+//                       int delta, a, d;
+//                       
+//                        System.out.println("insira a: ");
+//                     a = entrada.nextInt();
+//                     System.out.println("insira delta");
+//                     delta = entrada.nextInt();
+//                     
+//                     d = (int) Math.abs ((Math.sqrt(delta))/ a);
+//                     
+//                     System.out.println("resultado é:"+d);
+
+                                 //ATIVIDADE 12
+//                            
+//                       int a, b, c ,x0, fx0, f2x0;
+//                      
+//                     System.out.println("insira A:");  
+//                     a = entrada.nextInt();
+//                     System.out.println("insira b");
+//                     b = entrada.nextInt();
+//                     System.out.println("insira c: ");
+//                     c = entrada.nextInt();
+//                     System.out.println("insira x0: ");
+//                     x0 = entrada.nextInt();
+//                     
+//                     fx0 = (a*(x0*x0)) + (b*x0)+c;
+//                     
+//                     System.out.println("seu primeiro resultado é:" + fx0);
+//                              
+//                     f2x0 = (2*a)*(x0)+b;
+//                     
+//                     System.out.println("o segundo resultado é" + f2x0);
+
+
+
+                                 //ATIVIDADE 13
+                                 
+//                                 
+//                        int m, b, x1, x2, y1, y2, A;
+//                        
+//                     System.out.println("insira m:");  
+//                     m = entrada.nextInt();
+//                     System.out.println("insira b");
+//                     b = entrada.nextInt();
+//                     System.out.println("insira x1: ");
+//                     x1 = entrada.nextInt();
+//                     System.out.println("insira x2: ");
+//                     x2 = entrada.nextInt();
+//                        
+//                     
+//                     y1 = (m*x1)+b;
+//                     
+//                     y2 = (m*x2) + b;
+//                     
+//                     A = ((y1+y2) / 2) * (x2-x1);
+//                     
+//                     System.out.println("seu resultado: "+ A);
+//     }
     
-}
+// }
+ 
