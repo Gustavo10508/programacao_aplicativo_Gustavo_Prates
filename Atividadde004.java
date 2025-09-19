05178d7fde240caec0bb7388c2a04daa423529b3
Gustavo10508 <0001140340@senaimgaluno.com.br>
2025-09-18 21:32:15 -0300

    Atividade 04 Gustavo Prates

---
+++ Atividadde004.java
@@ -0,0 +1,417 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade04;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        
+                //Atividade 01
+        
+        
+//        double Numero;
+//        
+//        System.out.println("insira o numero: ");
+//        Numero = entrada.nextDouble();
+//        
+//        if  (Numero >= 0){
+//            System.out.println("Voce inseriu um numero positivo");
+//        }
+//        
+//        else{ System.out.println("Voce inseriu um numero negativo");
+//        }
+        
+        
+
+
+                  //Atividade 02
+                  
+                  
+//          double Numero;
+//        
+//        System.out.println("insira o numero: ");
+//        Numero = entrada.nextDouble();
+//        
+//        if  (Numero >=1){
+//            System.out.println("Voce inseriu um numero positivo");
+//            
+//        }
+//        
+//        
+//        if (Numero == 0){
+//            System.out.println("voce colocou zero");
+//        }
+//               
+//        if (Numero <0){ System.out.println("Voce inseriu um numero negativo");
+//        }
+        
+            
+    
+    
+                    //Atividade 03
+                    
+          
+                    
+//          double nota1, nota2, nota3,notaFinal;
+//          
+//          
+//           System.out.println("insira a Nota 1: ");
+//                     nota1 = entrada.nextInt();
+//                     System.out.println("insira a Nota 2");
+//                     nota2 = entrada.nextInt();
+//                     System.out.println("insira a Nota 3:");
+//                     nota3 = entrada.nextInt();
+//                     
+//                     
+//                     notaFinal = (nota1 + nota2 + nota3)/ (3); 
+//                     
+//                     if (notaFinal >= 7) {
+//                         System.out.println("ALUNO APROVADO");
+//                     } 
+//                     
+//                     else {
+//                         System.out.println("ALUNO REPROVADO");
+//                     }
+
+
+
+
+                      //Atividade 04
+//                      
+//                       double nota1, nota2, nota3,notaFinal;
+//          
+//          
+//           System.out.println("insira a Nota 1: ");
+//                     nota1 = entrada.nextInt();
+//                     System.out.println("insira a Nota 2");
+//                     nota2 = entrada.nextInt();
+//                     System.out.println("insira a Nota 3:");
+//                     nota3 = entrada.nextInt();
+//                     
+//                     
+//                     notaFinal = (nota1 + nota2 + nota3)/ (3); 
+//                     
+//                     if (notaFinal >7) {
+//                         System.out.println("ALUNO APROVADO");
+//                     } 
+//                     
+//                     if (notaFinal >=5 ||notaFinal <=7 ){
+//                        System.out.println("ALUNO DE RECUPERACAO");
+//                     }
+//                     
+//                     else {
+//                         System.out.println("ALUNO REPROVADO");
+////                     }
+
+
+
+                                   //Atividade 05
+                                   
+                                   
+//                       int numero;
+//                       
+//                       System.out.println("insira o numero: ");
+//                       numero = entrada.nextInt();
+//                       
+//                       if (numero %2==0){
+//                           System.out.println("seu numero e par");
+//                       }
+//                       
+//                       else {
+//                           System.out.println("Seu numero e impar: ");
+//                       }
+                       
+
+
+                                    //Atividade 06
+                                    
+                                    
+//                        int numero_1, numero_2,numero_3;
+//                      
+//                        
+//                     System.out.println("insira o primeiro numero: ");  
+//                     numero_1 = entrada.nextInt();
+//                     System.out.println("insira o segundo numero: ");
+//                     numero_2 = entrada.nextInt();
+//                     System.out.println("insira o terceiro numero: ");
+//                     numero_3 = entrada.nextInt();
+//                     
+//                    if (numero_1>numero_2 && numero_1 > numero_3 ){
+//                        
+//                        System.out.println(numero_1);
+//                    }
+//                    
+//                    if (numero_2>numero_1 && numero_2>numero_3 ){
+//                        System.out.println(numero_2);
+//                    }
+//                    
+//                    if (numero_3> numero_1 && numero_3>numero_2 ){
+//                        System.out.println(numero_3);
+//                    }
+
+
+
+                                    //Atividade 07
+                                    
+//                                  int idade;
+//                     
+//                     System.out.println("insira sua idade: ");
+//                     idade = entrada.nextInt();
+//                     
+//                     if (idade >=18) {
+//                         System.out.println("adulto");
+//                     } 
+//                     
+//                     if (idade >=14 && idade < 18 ){
+//                        System.out.println("JUVENIL");
+//                     }
+//                     if (idade >=9 && idade < 14 ){
+//                        System.out.println("INFANTIL");
+//                     }
+//                     
+//                     if (idade<9){
+//                         System.out.println("MIRIM");
+//                     }
+
+                               //Atividade 08
+
+                               
+//                        int num1,num2,adicao;
+//                        
+//                        System.out.println("insira o 1 numero: ");
+//                        num1 = entrada.nextInt();
+//                        
+//                        System.out.println("insira o 2 numero: ");
+//                        num2 = entrada.nextInt();
+//                        
+//                        adicao = (num1+num2);
+//                        
+//                        if (adicao > 20){
+//                            System.out.println(adicao+8);
+//                        }
+//                        if (adicao <= 20){
+//                            System.out.println(adicao-5);
+//                        }
+
+
+                              //Atividade 09
+                              
+//                         double valor_prod,valor_venda, lucro;
+//                         
+//                         System.out.println("insira o valor do produto: ");
+//                         valor_prod = entrada.nextDouble();
+//                         
+//                         lucro = valor_prod / 100;
+//                         
+//                         if (valor_prod <20){
+//                             valor_venda = (lucro*45)+ valor_prod;
+//                             System.out.println(valor_venda);
+//                         }
+//                         
+//                         if (valor_prod >=20){
+//                             valor_venda = (lucro*30)+valor_prod;
+//                             System.out.println(valor_venda);
+//                         }
+                       
+
+
+
+                                        //Atividade 10
+                                        
+                           
+//
+//                    double compra, maca;
+//                    int quantidade;
+//                    
+//                    System.out.println("Digite a quantidade de maçãs: ");
+//                    quantidade= entrada.nextInt();
+//                    
+//                    if (quantidade < 12) {
+//                        maca=0.25;
+//                        compra=quantidade*maca;
+//                        System.out.println("O valor total da compra de "+quantidade+" maçãs, será de: R$"+compra);
+//                    }
+//                    else {
+//                        maca=0.30;
+//                        compra=quantidade*maca;
+//                        System.out.println("O valor total da compra de "+quantidade+" maçãs, será de: R$"+compra);
+//                    }
+//                    
+
+
+
+
+                                                //Atvidade 11
+                    
+//                       int sexo;
+//                       double altura, peso;
+//                       
+//                       System.out.println("Digite seu sexo: \nDigite 1 para Feminino. \nDigite 2 para Masculino.");
+//                       sexo = entrada.nextInt();
+//                       
+//                       
+//                       switch (sexo) {
+//                           case 1:
+//                                System.out.println("Digite sua altura: ");
+//                                altura=entrada.nextDouble();
+//                                peso=(65.1*altura)-44.7;
+//                                System.out.println("Seu peso ideal é de: "+peso+" Kg.");
+//                                break;
+//                                
+//                           case 2:
+//                                 System.out.println("Digite sua altura: ");
+//                                 altura=entrada.nextDouble();
+//                                 peso=(72.7*altura)-58;
+//                                 System.out.println("Seu peso ideal é de: "+peso+" Kg.");
+//                                 break;
+//                           default:
+//                                 System.out.println("Digite apenas 1 para feminino ou 2 para mascolino.");
+                                 
+
+
+
+
+                                            //Atividade 12
+
+//                         int num;
+//                         
+//                            System.out.println("Digite o dia da semana usando somente numeros de 1 a 7: ");
+//                                num = entrada.nextInt();
+//                         
+//                            switch (num) {
+//                                case 1:
+//                                    System.out.println("Domingo é final de semana");
+//                                    break;
+//                                    
+//                                case 2:
+//                                    System.out.println("Segunda-Feira, dia util.");
+//                                    break;
+//                                    
+//                                case 3:
+//                                    System.out.println("Terça-Feira, dia util.");
+//                                    break;
+//                                case 4:
+//                                    System.out.println("Quarta-Feira, dia util.");
+//                                    break;
+//                                case 5:
+//                                    System.out.println("Quinta-Feira, dia util.");
+//                                    break;
+//                                case 6:
+//                                    System.out.println("Sexta-feira, dia util.");
+//                                    break;
+//                                case 7:
+//                                    System.out.println("Sabado é final de semana.");
+//                                    break;
+//                                default:
+//                                    System.out.println("Número digitado invalido");
+//                        }
+
+                                    
+
+
+                                               // Atvidade 13
+                                               
+                                               
+//
+//                            int codigo, quantidade;
+//                            double preco, total;
+//                            total=0;
+//                            System.out.println("Produtos disponiveis em estoque: \nDetergente -> R$1,59 \nEsponja -> R$5,59 \nLã de aço -> R$1,79");
+//                            System.out.println("Digite o código do produto desejado: \nDetergente -> Cod. 100 \nEsponja -> cod. 101 \nLã de Aço -> Cod. 102 \nOu digite 4 para exibir o total.");
+//                            codigo= entrada.nextInt();
+//                            switch (codigo) {
+//                                case 100:
+//                                    System.out.println("Você selecionou o Detergente! \nDigite a quantidade desejada: ");
+//                                    quantidade= entrada.nextInt();
+//                                    total=total+(1.59*quantidade);
+//                                    System.out.println("O valor total será de: R$"+total);
+//                                    break;
+//                                case 101:
+//                                    System.out.println("Você selecionou a Esponja! \nDigite a quantidade desejada: ");
+//                                    quantidade= entrada.nextInt();
+//                                    total=total+(4.59*quantidade);
+//                                    System.out.println("O valor total será de: R$"+total);
+//                                    break;
+//                                case 102:
+//                                    System.out.println("Você selecionou a Lã de Aço! \nDigite a quantidade desejada: ");
+//                                    quantidade= entrada.nextInt();
+//                                    total=total+(1.79*quantidade);
+//                                    System.out.println("O valor total será de: R$"+total);
+//                                    break;
+//                                case 4:
+//                                    System.out.println("O valor total será de: R$"+total);
+//                                    break;
+//                                default:
+//                                    System.out.println("Numero invalido!");
+//                                      
+//                            }
+
+
+
+
+                                                    //Atividade 14
+
+//                int operacao;
+//                double num1, num2, total;
+//                System.out.println("Vamos calcular. \nEscolha a operação: \n1 - SOMA \n2 - SUBTRAÇÃO \n3 - MULTIPLICAÇÃO \n4 - DIVISÃO");
+//                operacao= entrada.nextInt();
+//                
+//                switch (operacao) {
+//                    case 1:
+//                        System.out.println("Você escolheu a Soma! \nDigite o primeiro valor:");
+//                        num1= entrada.nextDouble();
+//                        System.out.println("Digite o segundo valor: ");
+//                        num2= entrada.nextDouble();
+//                        total=num1+num2;
+//                        System.out.println("O total será: "+total);
+//                        break;
+//                    case 2:
+//                        System.out.println("Você escolheu a Subtração! \nDigite o primeiro valor:");
+//                        num1= entrada.nextDouble();
+//                        System.out.println("Digite o segundo valor: ");
+//                        num2= entrada.nextDouble();
+//                        total=num1-num2;
+//                        System.out.println("O total será: "+total);
+//                        break;
+//                    case 3:
+//                        System.out.println("Você escolheu a Multiplicação! \nDigite o primeiro valor:");
+//                        num1= entrada.nextDouble();
+//                        System.out.println("Digite o segundo valor: ");
+//                        num2= entrada.nextDouble();
+//                        total=num1*num2;
+//                        System.out.println("O total será: "+total);
+//                        break;
+//                    case 4:
+//                        System.out.println("Você escolheu a Divisão! \nDigite o primeiro valor:");
+//                        num1= entrada.nextDouble();
+//                        System.out.println("Digite o segundo valor: ");
+//                        num2= entrada.nextDouble();
+//                        if (num2!=0) {
+//                        total=num1/num2;
+//                        System.out.println("O total será: "+total); 
+//                        break; }
+//                        if (num2==0) {
+//                            System.out.println("O Divisor não pode ser Zero!");
+//                        break; }
+//                    default :
+//                        System.out.println("Opção invalida!");
+//                }
+//                 
+
+//    }
+//    
+//}
