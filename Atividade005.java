d855e55d06d552631c7f815c54442285413e7383
Gustavo10508 <0001140340@senaimgaluno.com.br>
2025-09-24 20:28:54 -0300

    Create Atividade005.java

---
+++ Atividade005.java
@@ -0,0 +1,279 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade005;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade005 {
+
+   // Atividade01
+    public static void main(String[] args) {
+       // Scanner entrada = new Scanner (System.in);
+   
+    
+//            int i, num;
+//            i =0;
+//            System.out.println("escreva um numero: ");
+//            num = entrada.nextInt();
+//       
+//            while  (i <num){
+//              i = i+1;
+//            System.out.println(i);
+//            } 
+//    
+            
+            // Atividade02
+            
+           
+//            int  num, soma,qntd;
+//            num = 0;
+//            soma = 0;
+//            qntd =0;
+//           
+//             
+//            while (num != -1) {
+//            if  (num != -1) {
+//                System.out.println("escreva um numero: ");
+//                num = entrada.nextInt();
+//                soma = soma + num;
+//                qntd = qntd +1;
+//            }
+//           
+//            if  (num == -1) {
+//                soma = soma + 1;
+//                qntd = qntd - 1;
+//                System.out.println("A soma e:" + soma);
+//                System.out.println("a quantidade de numeros digitados foi:" + qntd);
+//            }
+//            
+//            }
+
+
+              //Atividade 03
+//              
+//             Scanner entrada = new Scanner (System.in);
+//             
+//            String senha = "";
+//            
+//           
+//            
+//             while (!senha.equals ("senai123")){
+//                
+//              System.out.println("Digite a senha correta");
+//              
+//              senha = entrada.nextLine();
+//              
+//              
+//              if (senha.equals ("senai123")){
+//              
+//                  System.out.println("A senha esta correta");
+//              }
+//              
+//            
+//              
+//             }
+
+//              Atividade 04
+
+               Scanner entrada = new Scanner (System.in);
+             
+               
+//               
+//             double num1, num2,total;
+//             int opcao;
+//             opcao= -1;
+              
+//              while (opcao!=0) {
+//             System.out.println("--Menu-- \n1-Somar \n2-Subtrair \n3- Multiplicar \n0-Sair ");
+//             System.out.println("digite a opcao desejada: ");
+//            
+//             opcao= entrada.nextInt();
+//             
+//            
+//                 switch (opcao) {
+//                     case 1 -> {
+//                         System.out.println("insira o primeiro numero");
+//                         num1 = entrada.nextDouble ();
+//                         
+//                         System.out.println("insira o segundo numero");
+//                         num2 = entrada.nextDouble ();
+//                         
+//                         total = num1 + num2;
+//                         
+//                         System.out.println("o valor total e: " + total);
+//                         break;
+//                       }
+//                         
+//                     case 2 -> {
+//                         System.out.println("insira o primeiro numero");
+//                         num1 = entrada.nextDouble ();
+//                         
+//                         System.out.println("insira o segundo numero");
+//                         num2 = entrada.nextDouble ();
+//                         
+//                         total = num1 - num2;
+//                         
+//                         System.out.println("o valor total e: " + total);
+//                         break;
+//                       }
+//                         
+//                     case 3 -> {
+//                         System.out.println("insira o primeiro numero");
+//                         num1 = entrada.nextDouble ();
+//                         
+//                         System.out.println("insira o segundo numero");
+//                         num2 = entrada.nextDouble ();
+//                         
+//                         total = num1 * num2;
+//                         
+//                         System.out.println("o valor total e: " + total);
+//                         break;
+//                       }
+//                     case 0 -> {System.out.println("Programa encerrado");
+//                    
+//                     }
+//                     default ->
+//                         System.out.println("essa opcao nao existe");
+//                 }
+//             
+//            
+//               
+//               
+//             }
+                     // Atividade 05
+             
+                        
+//             int n,i;
+//             double total;
+//             
+//             System.out.println("insira o numero: ");
+//             n =entrada.nextInt();
+//            
+//             i=n;       
+//             
+//             total = 1;
+//             
+//             
+//             if (n<=0){
+//                 System.out.println("Numero invalido!");
+//             }
+//             
+//             else{
+//                while (i>0){
+//             
+//                    total = i * total;
+//             
+//                    i = i - 1;
+//             
+//                    System.out.println("o valor e " + total);
+//             
+//             
+//             }}
+             
+                         //Atividade06
+                  
+//                System.out.print("Digite um número: ");
+//
+//                int num = entrada.nextInt();
+//
+//                int num1 = 0, num2 = 1;
+//                System.out.println("Sequência de Fibonacci: " + num1);
+//
+//                while (num2 <= num) {
+//                    System.out.println(" " + num2);
+//                    int temp = num1 + num2;
+//                    num1 = num2;
+//                    num2 = temp;
+//                            }
+                             
+            
+                 //Atividade07 
+//                  System.out.print("Digite um número n ≥ 2: ");
+//        int n = entrada.nextInt();
+//
+//        int i = 2;
+//        boolean primo = true;
+//
+//        while (i * i <= n) {
+//            if (n % i == 0) {
+//                primo = false;
+//                break;
+//            }
+//            i++;
+//        }
+//
+//        if (primo) {
+//            System.out.println(n + " é primo");
+//        } else {
+//            System.out.println(n + " não é primo");
+//        }
+    
+                 //atividade 08
+                 
+//                 int soma = 0;
+
+//        while (true) {
+//            System.out.print("Digite um número (negativo para encerrar): ");
+//            int num = entrada.nextInt();
+//
+//            if (num < 0) break;
+//            soma += num;
+//        }
+//
+//        System.out.println("Soma total = " + soma);
+//    }
+               
+
+                
+    
+    
+                    //Atividade 10
+                    
+                    
+//            System.out.print("Digite um número: ");
+//            int n = entrada.nextInt();
+//
+//            while (n != 1) {
+//                System.out.print(n + " ");
+//                if (n % 2 == 0) {
+//                n = n / 2;
+//              } else {
+//                n = 3 * n + 1;
+//            }
+//        }
+//            System.out.println("1");
+//                    
+                 
+
+
+
+
+
+
+
+
+                     
+             }          
+
+    }
+              
+   
+              
+
+
+
+
+
+
+    
+            
+    
+   
+
