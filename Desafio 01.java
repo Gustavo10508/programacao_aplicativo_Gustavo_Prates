234c6d2333ed92c1b786663bb60582a3c4a6f5e2
Gustavo Prates <0001140340@senaimgaluno.com.br>
2025-09-30 21:36:42 -0300

    Update Desafio 01.java

--- Desafio 01.java
+++ Desafio 01.java
@@ -4,6 +4,7 @@
  */
 package javaapplication10;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -12,22 +13,20 @@ import java.util.Scanner;
  */
 public class JavaApplication10 {
 
-    /**
-     * @param args the command line arguments
-     */
+    @SuppressWarnings("empty-statement")
     public static void main(String[] args) {
-        // TODO code application logic here
-        
         Scanner entrada = new Scanner(System.in);
 
         // Desafio JAVA 01
-        int opcao;
-        String opcao2;                  
-        double reais, dolar, deposito;
+        int opcao, indiExtrato;
+        String opcao2;
+        double reais, dolar, deposito, emprestimo, salario;
+        double[] extrato = new double[10];
 
         opcao = -1;
         reais = 100.00;
         dolar = 0.00;
+        indiExtrato = 0;
 
         while (opcao != 0) {
             System.out.println("""
@@ -69,22 +68,32 @@ public class JavaApplication10 {
 
                         if (deposito > 0 && deposito < 5000) {
                             reais = deposito + reais;
+
+                            if (indiExtrato < extrato.length) {
+                                extrato[indiExtrato] = deposito;
+                                indiExtrato++;
+                            } else {
+                                System.out.println("Extrato cheio! Não é possível armazenar mais depósitos.");
+                            }
+
                             System.out.println("Valor inserido com sucesso!");
+                            System.out.println("O valor depositado e de R$" + deposito + "\n");
+
                         } else {
                             System.out.println("""
                                                Valor invalido!!!
                                                O deposito deve ser maior que R$ 0,00 e ate R$ 5.000,00.
-                                               Digite novamente.""");
+                                               Digite novamente.\n""");
 
                             System.out.println("""
                                                Deseja voltar ao menu inicial, digite: (V)
-                                               Deseja encerrar o programa, digite: (E)""");
+                                               Deseja encerrar o programa, digite: (E)/n""");
                         }
                     } while (deposito <= 0 || deposito >= 5000);
 
                     System.out.println("""
                                        Deseja voltar ao menu inicial, digite: (V)
-                                       Deseja encerrar o programa, digite: (E)""");
+                                       Deseja encerrar o programa, digite: (E)\n""");
 
                     opcao2 = entrada.nextLine();
 
@@ -93,10 +102,44 @@ public class JavaApplication10 {
                         System.out.println("Sistema encerrado...");
                     }
                 }
-                
-                
-                
-                
+
+                case 3 -> {
+                    if (indiExtrato == 0) {
+                        System.out.println("Nenhum extrato");
+                    } else {
+                        for (int i = indiExtrato - 1; i >= 0; i--) {
+                            System.out.println((indiExtrato - i) + ") " + extrato[i]);
+                        }
+
+                        System.out.println("""
+                                           Deseja voltar ao menu inicial, digite: (V)
+                                           Deseja encerrar o programa, digite: (E)\n""");
+
+                        opcao2 = entrada.nextLine();
+
+                        if (opcao2.equalsIgnoreCase("E")) {
+                            opcao = 0;
+                            System.out.println("Sistema encerrado...");
+                        }
+                    }
+                }
+
+                case 4 -> {
+                    System.out.println("Insira o valor do emprestimo");
+                    emprestimo = entrada.nextDouble();
+
+                    if (emprestimo < 200 && emprestimo > 1000000) {
+                        System.out.println("Valor invalido\nO valor deve ser maior que 200 e menor que 100.000,00");
+                    }
+
+                    if (emprestimo >= 200 && emprestimo <= 1000000) {
+                        System.out.println("Insira o salario bruto");
+                        salario = entrada.nextDouble();
+                        System.out.println("insira o numero de parcelas");
+                    }
+
+                    
+                }
             }
         }
     }
