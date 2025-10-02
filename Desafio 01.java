02713e465d741bea8a1bbfd645b5073c2e923847
Gustavo Prates <0001140340@senaimgaluno.com.br>
2025-10-01 21:37:46 -0300

    Update Desafio 01.java

--- Desafio 01.java
+++ Desafio 01.java
@@ -5,6 +5,7 @@
 package javaapplication10;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -18,15 +19,16 @@ public class JavaApplication10 {
         Scanner entrada = new Scanner(System.in);
 
         // Desafio JAVA 01
-        int opcao, indiExtrato;
+        int opcao, indiExtrato, parcelas,protocolo;
         String opcao2;
-        double reais, dolar, deposito, emprestimo, salario;
+        double reais, dolar, deposito, emprestimo, salario = 0, divisao,porcento;
         double[] extrato = new double[10];
 
         opcao = -1;
         reais = 100.00;
         dolar = 0.00;
         indiExtrato = 0;
+       
 
         while (opcao != 0) {
             System.out.println("""
@@ -110,34 +112,77 @@ public class JavaApplication10 {
                         for (int i = indiExtrato - 1; i >= 0; i--) {
                             System.out.println((indiExtrato - i) + ") " + extrato[i]);
                         }
+                         System.out.println("""
+                                       Deseja voltar ao menu inicial, digite: (V)
+                                       Deseja encerrar o programa, digite: (E)""");
 
-                        System.out.println("""
-                                           Deseja voltar ao menu inicial, digite: (V)
-                                           Deseja encerrar o programa, digite: (E)\n""");
+                    opcao2 = entrada.nextLine();
 
-                        opcao2 = entrada.nextLine();
-
-                        if (opcao2.equalsIgnoreCase("E")) {
-                            opcao = 0;
-                            System.out.println("Sistema encerrado...");
-                        }
+                    if (opcao2.equalsIgnoreCase("E")) {
+                        opcao = 0;
+                        System.out.println("Sistema encerrado...");
+                    }
                     }
                 }
 
                 case 4 -> {
-                    System.out.println("Insira o valor do emprestimo");
+                    do{
+                    System.out.println("Insira o valor do emprestimo: ");
                     emprestimo = entrada.nextDouble();
-
-                    if (emprestimo < 200 && emprestimo > 1000000) {
-                        System.out.println("Valor invalido\nO valor deve ser maior que 200 e menor que 100.000,00");
+                    entrada.nextLine();
+                    if (emprestimo < 200 || emprestimo > 1000000) {
+                        System.out.println("Valor invalido\nO valor deve ser maior que 200 e menor que 100.000,00\n");
                     }
 
                     if (emprestimo >= 200 && emprestimo <= 1000000) {
-                        System.out.println("Insira o salario bruto");
-                        salario = entrada.nextDouble();
-                        System.out.println("insira o numero de parcelas");
+                        System.out.println("Insira o salario bruto: ");
+                        salario = entrada.nextDouble();}
+                        do{
+                        System.out.println("insira o numero de parcelas (6, 12, 18, 24, 30, 36, 40, 48, 56, 60 ou 72.): ");
+                        parcelas = entrada.nextInt();
+                        entrada.nextLine();
+                        
+                        
+                    if (parcelas == 6 || parcelas == 12 || parcelas == 18 || parcelas == 24 
+                        || parcelas == 30 || parcelas == 36 || parcelas == 40 
+                        || parcelas == 48 || parcelas == 56 || parcelas == 60 || parcelas == 72) {
+    
+                        divisao = emprestimo / parcelas;
+                        porcento = salario * 0.30;
+
+                    if (porcento > divisao) {
+                        Random random = new Random();
+                        protocolo = 100000 + random.nextInt(900000); 
+                        System.out.println("Emprestimo disponivel, entre em contato com a central e informe o numero de protocolo " + "EM"+protocolo);
+                    } else {
+                        System.out.println("Empréstimo indisponível (parcela maior que 30% do salário).");
                     }
 
+                    } else {
+                    System.out.println("Número de parcelas inválido...");
+                    }
+
+                        
+                    }while (parcelas != 6 && parcelas != 12 && parcelas != 18 && parcelas != 24 
+                             && parcelas != 30 && parcelas != 36 && parcelas != 40 
+                             && parcelas != 48 && parcelas != 56 && parcelas != 60 && parcelas != 72 );
+                        
+                     
+                       
+                    }while (emprestimo < 200 || emprestimo > 1000000);
+                    
+                     System.out.println("""
+                                       Deseja voltar ao menu inicial, digite: (V)
+                                       Deseja encerrar o programa, digite: (E)""");
+                    
+
+                    opcao2 = entrada.nextLine();
+
+                    if (opcao2.equalsIgnoreCase("E")) {
+                        opcao = 0;
+                        System.out.println("Sistema encerrado...");
+                    
+                    }
                     
                 }
             }
