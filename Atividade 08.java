80b4c646472ba3378bdc6b269fe653f03e59ab65
Gustavo Prates <0001140340@senaimgaluno.com.br>
2025-10-08 21:42:38 -0300

    Create Atividade 08.java

---
+++ Atividade 08.java
@@ -0,0 +1,95 @@
+
+package atividade008;
+
+import java.util.Scanner;
+
+
+public class Atividade008 {
+                    //QUESTÂO 01
+//    public static double calcDelt(double a, double b, double c){
+//        return (b*b) - (4*a*c);
+//    }
+    
+    
+                    //QUESTÃO 02
+//    public static int soma1 (int a, int b)
+//    {
+//        return (a+b);
+//    }
+//   public static int sub2 (int a, int b)
+//    {
+//        return (a-b);
+//    }
+//      public static int multi (int a, int b)
+//    {
+//        return (a*b);
+//    }
+//         public static int div (int a, int b)
+//    {
+//        return (a/b);
+//    }
+    
+                
+                //QUESTÂO 02
+    
+    
+    
+    public static void main(String[] args) {
+                    //QUESTÂO 01
+//        double delta1= calcDelt(2,5,9);
+//        double delta2= calcDelt(4,8,7);
+//        
+//        System.out.println(delta1);
+//        System.out.println(delta2);
+        
+
+                //QUESTÂO 02
+                Scanner entrada = new Scanner (System.in);
+//                int  num1, num2, op;
+//                
+//                
+//                System.out.println("==MENU==\n 1.Soma\n2.Subitração\n3.Multiplicação\n4.Divisão");
+//                op = entrada.nextInt();
+//                switch(op){
+//                        case 1 -> { System.out.println("insira o valor: ");
+//                        num1 = entrada.nextInt();
+//                            System.out.println("Insira o segundo valor: ");
+//                        num2 = entrada.nextInt();
+//                        int resul = soma1(num1,num2);
+//                            System.out.println(resul);}
+//                
+//                        
+//                         case 2 -> { System.out.println("insira o valor: ");
+//                        num1 = entrada.nextInt();
+//                            System.out.println("Insira o segundo valor: ");
+//                        num2 = entrada.nextInt();
+//                        int resul = sub2(num1,num2);
+//                            System.out.println(resul);}
+//                         
+//                         
+//                          case 3 -> { System.out.println("insira o valor: ");
+//                        num1 = entrada.nextInt();
+//                            System.out.println("Insira o segundo valor: ");
+//                        num2 = entrada.nextInt();
+//                        int resul = multi(num1,num2);
+//                            System.out.println(resul); }
+//                          
+//                          
+//                           case 4 -> { System.out.println("insira o valor: ");
+//                        num1 = entrada.nextInt();
+//                            System.out.println("Insira o segundo valor: ");
+//                        num2 = entrada.nextInt();
+//                        int resul = div(num1,num2);
+//                            System.out.println(resul);}
+//            
+//                        }
+    }
+}
+                
+                
+                
+                
+          
+        
+        
+   
