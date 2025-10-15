6995608644796414d81fd1d90d5ff81c85b5b026
Gustavo Prates <0001140340@senaimgaluno.com.br>
2025-10-15 19:13:17 -0300

    Create ClassExtratoB.java

---
+++ Desafio2.java/ClassExtratoB.java
@@ -0,0 +1,33 @@
+package javaapplication10;
+
+public class ExtratoBancario {
+    private final String[] extrato;
+    private int indice;
+
+    public ExtratoBancario(int tamanho) {
+        extrato = new String[tamanho];
+        indice = 0;
+    }
+
+    public void registrar(Movimentacao mov) {
+        if (indice < extrato.length) {
+            extrato[indice] = mov.toString();
+            indice++;
+        }
+    }
+
+    public void imprimir() {
+        if (indice == 0) {
+            System.out.println("Não há movimentações ainda.");
+        } else {
+            System.out.println("=== Extrato ===");
+            for (int i = 0; i < indice; i++) {
+                System.out.println((i + 1) + ") " + extrato[i]);
+            }
+        }
+    }
+
+    public boolean vazio() {
+        return indice == 0;
+    }
+}
