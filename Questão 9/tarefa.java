64d45acd75f7d66cc460dbbc4c2c817719872c60
Gustavo Prates <0001140340@senaimgaluno.com.br>
2025-10-20 19:01:00 -0300

    Create tarefa.java

---
+++ Questão 9/tarefa.java
@@ -0,0 +1,30 @@
+public class Tarefa {
+    private String descricao;
+    private boolean concluida;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+        this.concluida = false;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+
+    public String toString() {
+        String status = concluida ? "Concluída" : "Pendente";
+        return descricao + " - " + status;
+    }
+}
