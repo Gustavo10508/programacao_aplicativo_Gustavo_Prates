a8df22d989296b51f90a73a26e933cb038c4fef0
Gustavo Prates <0001140340@senaimgaluno.com.br>
2025-10-20 18:59:55 -0300

    Create livro.java

---
+++ Questão 9/livro.java
@@ -0,0 +1,40 @@
+public class Livro {
+    private String titulo;
+    private String autor;
+    private boolean emprestado;
+
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.emprestado = false;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public boolean isEmprestado() {
+        return emprestado;
+    }
+
+    public void setEmprestado(boolean emprestado) {
+        this.emprestado = emprestado;
+    }
+
+    public String toString() {
+        String status = emprestado ? "Emprestado" : "Disponível";
+        return titulo + " - " + autor + " (" + status + ")";
+    }
+}
